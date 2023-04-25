be0c2f778211fd4b29a8abab100a4f3d0fd0e6fb
ChiBao <99703812+NotAfraiddd@users.noreply.github.com>
2023-04-25 14:44:03 +0700

    CRUD với Node.js và Sequelize - Sử Dụng ORM Để Hiển Thị Dữ liệu

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,5 @@
 import db from '../models/index';
+import CRUDService from '../services/CRUD.JS';
 let getHomePage = async (req, res) => {
   try {
     let data = await db.User.findAll();
@@ -8,6 +9,26 @@ let getHomePage = async (req, res) => {
   }
 };
 
+let getCreate = (req, res) => {
+  return res.render('CRUD.ejs');
+};
+
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  return res.send('post CRUD from server');
+};
+
+let displayGetCRUD = async (req, res) => {
+  let data = await CRUDService.getAllUser();
+  return res.render('DisplayCRUD.ejs', {
+    dataTable: data,
+  });
+};
+
 module.exports = {
   getHomePage,
+  getCreate,
+  postCRUD,
+  displayGetCRUD,
 };
--- src/router/web.js
+++ src/router/web.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { getHomePage } from '../controller/homeController';
+import { getHomePage, getCreate, postCRUD, displayGetCRUD } from '../controller/homeController';
 let router = express.Router();
 
 const initWebRoute = (app) => {
   // ! khi muốn lấy thông tin về dùng GET
   router.get('/', getHomePage);
+  router.get('/create', getCreate);
+  router.post('/post-crud', postCRUD);
+  router.get('/get-crud', displayGetCRUD);
 
   return app.use('/', router);
 };
